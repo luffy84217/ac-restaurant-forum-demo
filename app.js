71fc3d87e87f20a5dd733e2171a3b664e01f3fd0
luffy84217 <luffy84217@gmail.com>
2019-08-25 16:06:06 +0800

    ready to deploy

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express')
-const handlebars = require('express-handlebars') // 引入 handlebars
+const handlebars = require('express-handlebars')
+const db = require('./models') // 引入資料庫
 const app = express()
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
+const methodOverride = require('method-override')
 const port = 3000
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
-app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(methodOverride('_method'))
+
+app.use('/upload', express.static(__dirname + '/upload'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+ db.sequelize.sync() // 跟資料庫同步
+ console.log(`Example app listening on port ${port}!`)
 })
+
+require('./routes')(app, passport)
