20035aa10bd8db6d12cb00f002854c885268e116
luffy84217 <luffy84217@gmail.com>
2019-09-09 16:25:31 +0800

    Architecture Refact

---
+++ controllers/api/adminController.js
@@ -0,0 +1,14 @@
+const db = require('../../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
+const adminService = require('../../services/adminService.js')
+
+const adminController = {
+  getRestaurants: (req, res) => {
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants', data)
+    })
+  }
+}
+module.exports = adminController
